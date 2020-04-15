f213613b52d9cf1103d2f2309d6f312eeab26169
Aswanth-Ramamoorthy <aswanthramamoorthy@gmail.com>
2020-04-15 15:20:39 -0400

    Create user_details.h

---
+++ Include/user_details.h
@@ -0,0 +1,8 @@
+/**@file user_details.h 
+ *@brief contains declaration for the function user_details that displays the details of the users after user_login .
+ the function is defination found in user_details.c 
+*/
+#ifndef _USER_DETAILS_H_
+#define _USER_DETAILS_H_
+extern int user_details(char* name);
+#endif 
