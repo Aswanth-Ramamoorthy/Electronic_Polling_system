98f4740f039c1e5368d4fdfee6e575d5ccc549ca
Aswanth-Ramamoorthy <aswanthramamoorthy@gmail.com>
2020-04-15 15:20:48 -0400

    Create intToString.h

---
+++ Include/intToString.h
@@ -0,0 +1,8 @@
+/**@file intToString.h 
+ *@brief contains declaration for the function intToString that converts intiger to string .
+ the function is defination found intToString.c 
+*/
+#ifndef _INTTOSTRING_H_
+#define _INTTOSTRING_H_
+extern char* intToString(int num, char* str);
+#endif 
