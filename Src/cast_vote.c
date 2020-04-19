3050a82c1a0b257305c1ec1770037750dba3ec37
somasundaramravindra <somasundaramravindra@cmail.carleton.ca>
2020-04-19 01:46:25 -0400

    Update cast_vote.c
    updated cast_vote  function

--- Src/cast_vote.c
+++ Src/cast_vote.c
@@ -2,13 +2,13 @@
 #include<stdio.h>
 #include<string.h>
 #include <stdlib.h>
-#pragma warning(disable : 4996)
-#include"getFileColSize.h"
-#include"replace_char.h"
-#include"getfield.h"
-#include "cast_vote.h"
-#include"intToString.h"
-int cast_vote(char* province) {
+#include"..include/getFileColSize.h"
+#include"..include/replace_char.h"
+#include"..include/getfield.h"
+#include "..include/cast_vote.h"
+#include"..include/intToString.h"
+int cast_vote(char* province) 
+{
 	FILE* fp;
 
 	char buf[1024];
@@ -18,17 +18,19 @@ int cast_vote(char* province) {
 	printf("\n------------------------------------------");
 	printf("\n----Type in the Candidate Id (e.g Candidate1) and press Enter to cast your vote----------");
 	printf("\n\n");
-	printf("Enter your Candidate Id here:");
-	scanf("%s", &candidate_id);
+	printf("Enter your preferred Candidate Id here:");
+	scanf("%s", &candidate_id);//inputs unique candidate_id from user
 	if ((strcmp(candidate_id, "Candidate1") == 0) || (strcmp(candidate_id, "Candidate2") == 0) || (strcmp(candidate_id, "Candidate3") == 0) || (strcmp(candidate_id, "Candidate4") == 0) || (strcmp(candidate_id, "Candidate5") == 0))
 	{
-		fp = fopen("vote_count.csv", "r+");
+		fp = fopen("../data/vote_count.csv", "r+");
 
-		if (!fp) {
+		if (!fp) 
+		{
 			printf("Can't open file\n");
+			return 0;//returns when file does not exist
 		}
 
-		FILE* tempVoteFile = fopen("temp.csv", "w+");
+		FILE* tempVoteFile = fopen("../data/temp.csv", "w+");
 		int row = 0;
 		while (fgets(buf, 1024, fp))
 		{
@@ -36,41 +38,53 @@ int cast_vote(char* province) {
 			char* tmp2 = strdup(buf);
 			int fileColSize = getFileColSize(tmp2);
 			static int selectedCandidateColIndexInFile;
-			if (row == 0) {
+			if (row == 0)
+			{
 				char* tempHeaderRow = getfield(tmp, 1);
 				fprintf(tempVoteFile, tempHeaderRow);
 				char* token = strtok(tempHeaderRow, ",");
 				int headerCol = 0;
-				while (token != NULL) {
-					if (strcmp(token, candidate_id) == 0) {
+				while (token != NULL)
+				{
+					if (strcmp(token, candidate_id) == 0)
+					{
 						selectedCandidateColIndexInFile = headerCol;
-						break;
+						//break;
+						return 1;//returns when candidate_id matches
 					}
 					token = strtok(NULL, ",");
 					headerCol++;
+					else
+                    {
+					    return -1;//returns when candidate_id does not match
+                    }
 				}
 				row++;
 				continue;
 			}
+
 			printf("\n");
 			char* tempRow = getfield(tmp, 1);
 			char* token = strtok(tempRow, ",");
 			const char* fieldProvince = token;
-			if (strcmp(fieldProvince, province) == 0) {
+			if (strcmp(fieldProvince, province) == 0)
+			{
 				int col = 1;
 				while (token != NULL) {
-					if (selectedCandidateColIndexInFile == (col - 1)) {
+					if (selectedCandidateColIndexInFile == (col - 1))
+					{
 						int tempToken = atoi(token);
 						tempToken++;
 						char str[20];
 						if (intToString(tempToken, str) != NULL)
 						{
-							fprintf(tempVoteFile, strcat(str, ","));
+							fprintf(tempVoteFile, strcat(str, ","));//writes incremented vote to temp.csv file
 						}
 
 					}
 					else {
-						if (col == fileColSize) {
+						if (col == fileColSize) 
+						{
 							fprintf(tempVoteFile, "%s%s", token, "");
 						}
 						else {
@@ -81,14 +95,17 @@ int cast_vote(char* province) {
 					token = strtok(NULL, ",");
 					col++;
 				}
-			}
-			else {
+			} else 
+			{
 				int col = 1;
-				while (token != NULL) {
-					if (col == fileColSize) {
+				while (token != NULL)
+				{
+					if (col == fileColSize)
+					{
 						fprintf(tempVoteFile, "%s%s", token, "");
-					}
-					else {
+					} else
+					
+					{
 						fprintf(tempVoteFile, "%s%s", token, ",");
 					}
 					token = strtok(NULL, ",");
@@ -102,16 +119,20 @@ int cast_vote(char* province) {
 		printf("\n");
 		printf("Vote casted for your candidate: ");
 		printf("%s", candidate_id);
-		printf(" successfully.Thank you!!!");
+		printf(" successfully.");
+		printf("\n\nThank you!!!");
+		printf("\n\n");
+		printf("Press any key to exit");
+		getch();
 		fclose(tempVoteFile);
-		remove("vote_count.csv");
-		rename("temp.csv", "vote_count.csv");
-		return 0;
+		remove("../data/vote_count.csv");//deletes vote_count.csv
+		rename("../data/temp.csv", "../data/vote_count.csv");//renames temp.csv to vote_count.csv
+		//return 0;
 	}
 	else
 	{
-		printf("Please enter a valid candidate number as given in the hint");
-		return cast_vote(province);
+		printf("Please enter a valid candidate number as given in the e.g");
+		printf("\n\n");
+		cast_vote(province);
 	}
-	return 0;
 }
