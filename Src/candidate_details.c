9a388623abb9269bb5f449a2a26151dce0ebe523
priyangasoundararaja <61341785+priyangasoundararaja@users.noreply.github.com>
2020-04-19 01:27:50 -0400

    Update candidate_details.c
    Updated code for candidate_details function

--- Src/candidate_details.c
+++ Src/candidate_details.c
@@ -2,7 +2,6 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#pragma warning(disable : 4996)
 #include "getfield.h"
 #include"candidate_details.h"
 int candidate_details(char* province)
@@ -11,19 +10,21 @@ int candidate_details(char* province)
 	printf("\n==========================================");
 	printf("\nCANDIDATES INFORMATION");
 	printf("\n------------------------------------------");
-	//fp = fopen("userdetails.csv", "r");
-	FILE* fp = fopen("candidate_details.csv", "r");
-	if (!fp) {
+	FILE* fp = fopen("../data/candidate_details.csv", "r");
+	if (!fp) 
+	{
 		printf("Can't open file\n");
+                return 0;//returns when file does not exist
 	}
 
 	int row = 0;
-	//removing carriage return and new line character - due to getchar() ???
+	//removing carriage return and new line character - due to getchar() 
 	province = strtok(province, "\r\n");
 
 	while (fgets(buf, 1024, fp))
 	{
-		if (row == 0) {
+		if (row == 0)
+		{
 			row++;
 			continue;
 		}
@@ -35,17 +36,21 @@ int candidate_details(char* province)
 		char* token = strtok(tempRow, ",");
 		const char* fieldProvince = token;
 
-		if (strcmp(fieldProvince, province) == 0) {
+		if (strcmp(fieldProvince, province) == 0)//compares province of logged in user with that of candidate_details.csv file
+		{
 			int col = 1;
-			while (token != NULL) {
-				if (col == 1) {
+			while (token != NULL)
+			{
+				if (col == 1)
+				{
 					printf("Below are your candidates for your Province:");
 					printf("%s", token);
 					printf("\n--------------------------");
 					col++;
 					continue;
 				}
-				else if (col > 2) {
+				else if (col > 2) 
+				{
 					printf("\nCandidate");
 					printf("%d", col - 2);
 					printf(": ");
@@ -63,6 +68,9 @@ int candidate_details(char* province)
 	}
 
 	fclose(fp);
+	printf("\nPress Enter to vote...!!");
+	getch();
+	printf("\n\n\n");
 	int vote_casted = cast_vote(province);
 	return vote_casted;
 }
