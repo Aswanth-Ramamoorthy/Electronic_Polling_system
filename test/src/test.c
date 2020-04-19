7b55d42b17e0814523ab3bc38f21d72c5d3e37bf
priyangasoundararaja <61341785+priyangasoundararaja@users.noreply.github.com>
2020-04-19 04:48:34 -0400

    Update test.c
    Code for test main

--- test/src/test.c
+++ test/src/test.c
@@ -19,8 +19,33 @@
 #define itoa(x)
 #pragma warning(disable:4996)
 
-void main()
+int test_main()
 {
-    int test_cases();
+    int n;
+    printf("\n\t\t\t\t==========================================");
+    printf("\n\t\t\t\tWELCOME TO THE PRESIDENTIAL ELECTIONS 2020");
+    printf("\n\t\t\t\t==========================================");
+    printf("\n\nInstructions:\n\n");
+    printf("1.You can vote only once.\n2.Login using your User-ID and Password.\n3.Proceed to vote if your information displayed is correct.\n4.Please be fair and impartial in voting");
+    printf("\n\n\t\t\t\tIf you agree Press Enter to proceed...!!");
+    if (getch() == 13)
 
+        XY:
+        printf("\n\n\n\t\t\t\t1. Admin Login\t\t2. User Login");
+    printf("\n\n\n\t\t\t\t\tENTER YOUR CHOICE: ");
+    scanf("%d", &n);
+    switch (n)
+    {
+        case 1:
+            admin_login();
+            break;
+        case 2:
+            user_login();
+            break;
+        default: printf("\n\n\t\t\t\tNO MATCH FOUND");
+            printf("\n\n\t\t\tPress Enter to re-Enter the choice");
+            if(getch() == 13)
+                goto XY;
+    }
+    return 0;
 }
