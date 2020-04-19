c1dc712f42c917847994b1946dd62fb8e64f270f
vigneshbalaj <61341844+vigneshbalaj@users.noreply.github.com>
2020-04-19 01:14:55 -0400

    Update user_login.c
    Updated code for user_login function

--- Src/user_login.c
+++ Src/user_login.c
@@ -1,34 +1,36 @@
 #include<conio.h>
 #include<stdio.h>
 #include<string.h>
-#include <stdlib.h>
-#include "global.h"
-#include"user_details.h"
+#include"..include/user_login.h"
+#include"..include/user_details.h"
 int user_login()
 {
+	static int i = 0;
 	char username[15];
 	char password[12];
 	char* str[1024];
-	FILE* fp = fopen("auth.csv", "r");
-
+	FILE* fp = fopen("../data/auth.csv", "r");//file containing user authentication details
+	printf("--*--*--*--*--*--*--*--*--*");
+	printf("\n\tUSER LOGIN\n");
+	printf("--*--*--*--*--*--*--*--*--*\n");
 	printf("Enter your username:\n");
-	scanf("%s", &username);
+	scanf("%s", &username);//fetching input from user
 
 	printf("Enter your password:\n");
-	scanf("%s", &password);
 	for (i = 0; i < 6; i++)
 	{
 		password[i] = getch();
-		printf("*");
+		printf("*");//to hide the password on input screen
 
 	}
 	password[i] = '\0';
 	char string[1024];
 	int row_count = 0;
 	int field_count = 0, flag = 0;
-	if (!fp) {
+	if (!fp) 
+	{
 		printf("Can't open file\n");
-		return 0;
+		return 0;//returns when file does not exist
 	}
 	while (fgets(string, 1024, fp) && flag == 0)
 	{
@@ -39,13 +41,13 @@ int user_login()
 			continue;
 		}
 		char* field = strtok(string, ",");
-		if (strcmp(field, username) == 0)
+		if (strcmp(field, username) == 0)//compares entered username to that of auth.csv file
 		{
 			field = strtok(NULL, ",");
-			if (strcmp(field, password) == 0)
+			if (strcmp(field, password) == 0)//compares entered password to that of auth.csv file
 			{
-				while (field) {
-					printf("%s", field);
+				while (field) 
+				{
 					flag++;
 					field_count++;
 					break;
@@ -53,21 +55,25 @@ int user_login()
 			}
 		}
 	}
-	if (flag == 0) {
+	if (flag == 0)
+	{
 		printf("\nStatus:Invalid login");
 		printf("\n");
-                printf("\n\n\t  (PRESS [Y] TO RE-LOGIN)");
+		printf("\n\n\t\t\t\t  (PRESS [Y] TO RE-LOGIN)");
 		if (getch() == 'y' || getch() == 'Y')
-		system("clear");
-
+		user_login();//making the function available for user to retry
+        fclose(fp);
+        return -1;//returns when login fails
 	}
 	else
 	{
 		printf("\nStatus:Successful Login");
-		//const int secondary_authentication = second_auth(username);
-		//return secondary_authentication;
-		user_details(username);
+		printf("\n\n");
+		printf("\nPress enter to view your details");
+		getch();
+		user_details(username);//this function displays details of the user
+        fclose(fp);
+        return 1;//returns when login succeeds
 	}
-	fclose(fp);
-
 }
+
