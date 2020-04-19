709bbe2e265771d282253ad78f9d95292a083d5b
Aswanth-Ramamoorthy <aswanthramamoorthy@gmail.com>
2020-04-19 01:33:24 -0400

    Update user_details.c
    updated source code for function user_details

--- Src/user_details.c
+++ Src/user_details.c
@@ -2,10 +2,8 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#pragma warning(disable : 4996)
-
-#include"user_details.h"
-//#include"candidate_details.c"
+#include"..include/user_details.h"
+#include"..include/candidate_details.h"
 int user_details(char* name)
 {
 	char buf[1024];
@@ -15,63 +13,76 @@ int user_details(char* name)
 	printf("\n========================");
 	printf("\nUSER INFORMATION");
 	printf("\n========================");
-	FILE* fp = fopen("userdetails.csv", "r");
-	if (!fp) {
+	FILE* fp = fopen("../data/userdetails.csv", "r");//file containing details of all users or voters
+	if (!fp) 
+	{
 		printf("Can't open file\n");
-		return 0;
+		return 0;//returns when file does not exist
 	}
 	while (fgets(buf, 1024, fp))
 	{
 		col = 0;
 		row++;
 
-		if (row == 1) {
+		if (row == 1)
+		{
 			continue;
 		}
 		char* field = strtok(buf, ",");
 		printf("\n");
 		char* string = field;
 		string = strtok(NULL, ",");
-		if (strcmp(field, name) == 0)
+		if (strcmp(field, name) == 0)//compares username fetched from previous function with that of user_details file
 		{
 			while (field)
 			{
-				if (col == 0) {
-					printf("First Name:\t");
+				if (col == 0)
+				{
+					printf("First Name:\t");//prints value fetched from file
 				}
-				if (col == 1) {
-					printf("Last Name:\t");
+				if (col == 1)
+				{
+					printf("Last Name:\t");//prints value fetched from file
 					printf("%s\n", string);
 					col++;
 				}
-				if (col == 2) {
-					printf("Age:\t");
+				if (col == 2)
+				{
+					printf("Age:\t");//prints value fetched from file
 
 				}
-				if (col == 3) {
-					printf("Phone:\t");
+				if (col == 3)
+				{
+					printf("Phone:\t");//prints value fetched from file
 				}
-				if (col == 4) {
-					printf("Postal code:\t");
+				if (col == 4)
+				{
+					printf("Postal code:\t");//prints value fetched from file
 				}
-				if (col == 5) {
-					printf("Province:\t");
+				if (col == 5)
+				{
+					printf("Province:\t");//prints value fetched from file
 					printf("%s\n", field);
 					printf("\n\nPress Enter to view the candidates in your Province...!!");
 					getch();
-					candidate_details(field);
+					candidate_details(field);//passing province of logged in user to restrict from voting nominees in other provinces
 					break;
 				}
-				if (col < 5 && col != 1) {
+				if (col < 5 && col != 1)
+				{
 					printf("%s\n", field);
 					field = strtok(NULL, ",");
 					col++;
 				}
 			}
 			printf("\n");
+			return 1;//when details of user exist
 		}
+		else
+        {
+		    return -1;//when user not found
+        }
 	}
 
 	fclose(fp);
-	return 0;
 }
