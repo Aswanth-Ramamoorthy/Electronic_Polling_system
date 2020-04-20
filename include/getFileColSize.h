e656ab8a234e08d48c522567618ce4cb20bdf022
Aswanth-Ramamoorthy <aswanthramamoorthy@gmail.com>
2020-04-20 04:01:15 -0400

    Create getFileColSize.h

---
+++ include/getFileColSize.h
@@ -0,0 +1,13 @@
+/**
+ * @file getFileColSize.h
+ *
+ * @brief Implementation of getting a CSV file column size.
+ *
+ *
+ * @author Aswanth Ramamoorthy-aswanthramamoorthy@cmail.carleton.ca
+ *
+ **/
+#ifndef _GETFILECOLSIZE_H_
+#define _GETFILECOLSIZE_H_
+extern int getFileColSize(char* tmp);
+#endif 
