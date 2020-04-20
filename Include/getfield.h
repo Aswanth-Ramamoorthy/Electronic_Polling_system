5628c6ea1c81683ce98feb61a38ea29ddad3ac3b
Aswanth-Ramamoorthy <aswanthramamoorthy@gmail.com>
2020-04-20 03:59:04 -0400

    Delete getfield.h

--- Include/getfield.h
+++
@@ -1,8 +0,0 @@
-/**@file getfield.h 
- *@brief contains declaration for the function getfield that gets a fiels in CSV .
- the function is defination found in getfield.c 
-*/
-#ifndef _GETFIELD_H_
-#define _GETFIELD_H_
-extern char* getfield(char* line, int num);
-#endif 
