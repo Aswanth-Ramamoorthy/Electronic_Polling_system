e1fce0e60c06901d676e945d7cc9232c74a72d84
Aswanth-Ramamoorthy <aswanthramamoorthy@gmail.com>
2020-04-20 03:59:14 -0400

    Delete global.h

--- Include/global.h
+++
@@ -1,9 +0,0 @@
-/**@file global.h 
- *@brief contains all the global declarations .
- the function is defination found in getfield.c 
-*/
-#ifndef _GLOBAL_H_
-#define _GLOBAL_H_
-extern int i = 0;
-extern int n;
-#endif
