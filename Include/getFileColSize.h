9abae8e224642b741a942ea297f596f5974d77f9
Aswanth-Ramamoorthy <aswanthramamoorthy@gmail.com>
2020-04-20 03:59:11 -0400

    Delete getFileColSize.h

--- Include/getFileColSize.h
+++
@@ -1,8 +0,0 @@
-/**@file getfilecolsize.h 
- *@brief contains declaration for the function getfilecolsize that gets the column size of the CSV file .
- the function is defination found in getFileColSize.c 
-*/
-#ifndef _GETFILECOLSIZE_H_
-#define _GETFILECOLSIZE_H_
-extern int getFileColSize(char* tmp);
-#endif 
