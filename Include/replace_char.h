2673d1df77a4d8b9386f99fb6bd583339b7beaaa
Aswanth-Ramamoorthy <aswanthramamoorthy@gmail.com>
2020-04-20 03:59:24 -0400

    Delete replace_char.h

--- Include/replace_char.h
+++
@@ -1,8 +0,0 @@
-/**@file replace_char.h 
- *@brief contains declaration for the function replace_char that find and replace a char value .
- the function is defination found in replace_char.c 
-*/
-#ifndef _REPLACE_CHAR_H_
-#define _REPLACE_CHAR_H_
-extern char* replace_char(char* str, char find, char replace);
-#endif 
