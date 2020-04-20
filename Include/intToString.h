aec7cee65d788f1420c40f4d7892e207f8ff5977
Aswanth-Ramamoorthy <aswanthramamoorthy@gmail.com>
2020-04-20 03:59:19 -0400

    Delete intToString.h

--- Include/intToString.h
+++
@@ -1,8 +0,0 @@
-/**@file intToString.h 
- *@brief contains declaration for the function intToString that converts intiger to string .
- the function is defination found intToString.c 
-*/
-#ifndef _INTTOSTRING_H_
-#define _INTTOSTRING_H_
-extern char* intToString(int num, char* str);
-#endif 
