aaa54c49cff8fa22781cfe33acf13df9bc077a06
priyangasoundararaja <61341785+priyangasoundararaja@users.noreply.github.com>
2020-04-20 06:02:59 -0400

    Update candidate_details.h

--- include/candidate_details.h
+++ include/candidate_details.h
@@ -1,4 +1,26 @@
+/**
+ * @file candidate_details.h
+ *
+ * @brief Implementation of Candidate_details function to display the candidates contesting in
+ * particular province
+ * 
+ *
+ * @author Priyanga Soundararajan - priyangasoundararaja@cmail.carleton.ca
+ * 
+ * 
+ **/
 #ifndef _CANDIDATE_DETAILS_H_
 #define _CANDIDATE_DETAILS_H_
+/** 
+ * @brief The function displays a list of candidates present in the file. 
+ *
+ * @param[in] The function takes “province” as input parameter. 
+ *
+ * @param[out] The function returns “candidate_number” which is the 
+ * candidate number provided as input by the user. 
+ *
+ * @return Return 0 when file can't open.
+ *
+ **/
 extern int candidate_details(char* province);
 #endif 
